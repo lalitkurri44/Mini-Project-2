149ac3408541793348b51b9629570cde4c893062
Lasya63 <lasyakorandla11@gmail.com>
2025-03-07 06:59:34 +0530

    Add files via upload

---
+++ admin_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin page</title>
+ <!-- custom css file link -->
+  < link rel="stylesheet" href="css">   
+</head>
+<body>
+ <div class="container">
+    <div class="content">
+        <h3>hi, <span>admin</span></h3>
+        <h1>welcome <span></span><h1>
+         <p>This is an admin page </p>
+         <a href="loginform.php"class="btn">login</a>     
+         <a href="registerform.php"class="btn">register</a>  
+         <a href="logout.php"class="btn">logout</a>    
+         
+</body>
+</html>
